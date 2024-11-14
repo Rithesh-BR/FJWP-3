7b481a1311b31213cf95fec0af25d95180c66b06
Rithesh-BR <154339474+Rithesh-BR@users.noreply.github.com>
2024-11-14 21:40:24 +0530

    Add files via upload

---
+++ FJWP-3/Simpleif.java
@@ -0,0 +1,17 @@
+package rithesh;
+
+public class Simpleif {
+
+	public static void main(String[] args) {
+		System.out.println("start");
+		int num= 1;
+		
+		if(num <= 10)
+		{
+			System.out.println(num +"is lesser than or equal to 10");
+		}
+		System.out.println("End");
+
+	}
+
+}
